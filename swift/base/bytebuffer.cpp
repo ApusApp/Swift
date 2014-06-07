58bf90e72a70d4df7f4b7971cda27f56cb82f742
chenyongwang <chen.yongwang@apusapp.com>
2014-06-07 13:40:46 +0800

    Add ByteBuffer class
    ByteBuffer can storage specifice type, can use it to serialize and
    deserialize

---
+++ swift/base/bytebuffer.cpp
@@ -0,0 +1,316 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <string.h>
+#include <algorithm>
+
+#include "swift/base/bytebuffer.h"
+
+namespace swift {
+
+// public
+ByteBuffer::ByteBuffer ()
+{
+    Initialize (nullptr, kDefaultSize, ByteOrder::BYTE_ORDER_HOST);
+}
+
+// public
+ByteBuffer::~ByteBuffer ()
+{
+    if (nullptr != buffer_) {
+        delete [] buffer_;
+        buffer_ = nullptr;
+    }
+}
+
+// public
+ByteBuffer::ByteBuffer (const char *buffer, 
+                        size_t length, 
+                        ByteOrder byte_order)
+{   
+    Initialize (buffer, length, byte_order);
+}
+
+// public
+ByteBuffer::ByteBuffer (const char *buffer, size_t length)
+{
+    Initialize (buffer, length, ByteOrder::BYTE_ORDER_HOST);
+}
+
+// public
+ByteBuffer::ByteBuffer (const char *buffer)
+{
+    Initialize (buffer, strlen (buffer), ByteOrder::BYTE_ORDER_HOST);
+}
+
+// public
+ByteBuffer::ByteBuffer (ByteOrder byte_order)
+{
+    Initialize (nullptr, kDefaultSize, byte_order);
+}
+
+ByteBuffer::ByteBuffer (ByteBuffer&& other)
+    : size_ (other.size_)
+    , start_ (other.start_)
+    , end_ (other.end_)
+    , version_ (other.version_)
+    , byte_order_ (other.byte_order_)
+{
+    if (nullptr != buffer_) {
+        delete [] buffer_;
+    }
+
+    buffer_ = other.buffer_;
+    other.buffer_ = nullptr;
+    other.size_ = 0;
+    other.start_ = 0;
+    other.end_ = 0;
+    other.version_ = 0;
+}
+
+ByteBuffer& ByteBuffer::operator= (ByteBuffer&& rhs)
+{
+    if (&rhs != this) {
+        if (nullptr != buffer_) {
+            delete [] buffer_;
+        }
+        buffer_ = rhs.buffer_;
+        rhs.buffer_ = nullptr;
+
+        size_ = rhs.size_;
+        rhs.size_ = 0;
+
+        start_ = rhs.start_;
+        rhs.start_ = 0;
+
+        end_ = rhs.end_;
+        rhs.end_ = 0;
+
+        version_ = rhs.version_;
+        rhs.version_ = 0;
+    }
+
+    return *this;
+}
+
+// private
+void ByteBuffer::Initialize (const char *buffer, 
+                             size_t size, 
+                             ByteOrder byte_order)
+{
+    version_ = 0;
+    size_ = size;
+    start_ = 0;
+    byte_order_ = byte_order;
+    buffer_ = new char[size_];
+    memset (buffer_, 0, size_);
+
+    if (nullptr != buffer) {
+        end_ = size_;
+        memcpy (buffer_, buffer, end_);
+    }
+    else {
+        end_ = 0;
+    }
+}
+
+// public
+bool ByteBuffer::SetReadPosition (const ByteBuffer::ReadPosition& position)
+{
+    if (position.version_ != version_) {
+        return false;
+    }
+
+    start_ = position.start_;
+    return true;
+}
+
+// public
+bool ByteBuffer::ReadUInt16 (uint16_t *val)
+{
+    if (nullptr == val) {
+        return false;
+    }
+
+    uint16_t v;
+    if (ReadBytes (reinterpret_cast<char*>(&v), 2)) {
+        *val = (ByteOrder::BYTE_ORDER_NETWORKER == byte_order_) ?
+                ByteOrderHelper::NetworkToHost16 (v) :
+                v;
+        return true;
+    }
+    
+    return false;
+}
+
+// public
+bool ByteBuffer::ReadUInt32 (uint32_t *val)
+{
+    if (nullptr == val) {
+        return false;
+    }
+
+    uint32_t v;
+    if (ReadBytes (reinterpret_cast<char*>(&v), 4)) {
+        *val = (ByteOrder::BYTE_ORDER_NETWORKER == byte_order_) ?
+                ByteOrderHelper::NetworkToHost32 (v) :
+                v;
+        return true;
+    }
+    
+    return false;
+}
+
+// public
+bool ByteBuffer::ReadUInt64 (uint64_t *val)
+{
+    if (nullptr == val) {
+        return false;
+    }
+
+    uint64_t v;
+    if (ReadBytes (reinterpret_cast<char*>(&v), 8)) {
+        *val = (ByteOrder::BYTE_ORDER_NETWORKER == byte_order_) ?
+                ByteOrderHelper::NetworkToHost64 (v) :
+                v;
+        return true;
+    }
+
+    return false;
+}
+
+// public
+bool ByteBuffer::ReadBytes (char *val, size_t len)
+{
+    if (len > Length ()) {
+        return false;
+    }
+    else {
+        memcpy (val, buffer_ + start_, len);
+        start_ += len;
+    }
+
+    return true;
+}
+
+// public
+bool ByteBuffer::ReadString (std::string *val, size_t len)
+{
+    if (nullptr == val || len > Length ()) {
+        return false;
+    }
+
+    val->append (buffer_ + start_, len);
+    start_ += len;
+
+    return true;
+}
+
+// public
+void ByteBuffer::WriteUInt16 (uint16_t val)
+{
+    uint16_t v = (ByteOrder::BYTE_ORDER_NETWORKER == byte_order_) ?
+                  ByteOrderHelper::HostToNetwork16 (val) :
+                  val;
+    WriteBytes (reinterpret_cast<const char*>(&v), 2);
+}
+
+// public
+void ByteBuffer::WriteUInt32 (uint32_t val)
+{
+    uint32_t v = (ByteOrder::BYTE_ORDER_NETWORKER == byte_order_) ?
+                  ByteOrderHelper::HostToNetwork32 (val) :
+                  val;
+    WriteBytes (reinterpret_cast<const char*>(&v), 4);
+}
+
+// public
+void ByteBuffer::WriteUInt64 (uint64_t val)
+{
+    uint64_t v = (ByteOrder::BYTE_ORDER_NETWORKER == byte_order_) ?
+                  ByteOrderHelper::HostToNetwork64 (val) :
+                  val;
+    WriteBytes (reinterpret_cast<const char*>(&v), 8);
+}
+
+// public
+void ByteBuffer::WriteBytes (const char* val, size_t len)
+{
+    memcpy (ReserveWriteBuffer (len), val, len);
+}
+
+// public
+void ByteBuffer::Resize (size_t size)
+{
+    size_t len = std::min (end_ - start_, size);
+    if (size < size_) {
+        memmove (buffer_, buffer_ + start_, len);
+    }
+    else {
+        size_ = std::max (size, 3 * size_ / 2);
+        char *new_buffer = new char[size_];
+        memset (new_buffer, 0, size_);
+        memcpy (new_buffer, buffer_ + start_, len);
+        delete [] buffer_;
+        buffer_ = new_buffer;
+    }
+
+    start_ = 0;
+    end_ = len;
+    ++version_;
+}
+
+// public
+char* ByteBuffer::ReserveWriteBuffer (size_t len)
+{
+    if (Length () + len > Capacity ()) {
+        Resize (Length () + len);
+    }
+
+    char *start = buffer_ + end_;
+    end_ += len;
+    return start;
+}
+
+// public
+bool ByteBuffer::Consume (size_t size)
+{
+    if (size > Length ()) {
+        return false;
+    }
+
+    start_ += size;
+    return true;
+}
+
+// public
+void ByteBuffer::Clear ()
+{
+    memset (buffer_, 0, size_);
+    start_ = end_ = 0;
+    ++version_;
+}
+
+void ByteBuffer::Swap (ByteBuffer& other)
+{
+    using std::swap;
+    swap (buffer_, other.buffer_);
+    swap (size_, other.size_);
+    swap (start_, other.start_);
+    swap (end_, other.end_);
+    swap (version_, other.version_);
+    swap (byte_order_, other.byte_order_);
+}
+
+} // namespace swift
---
+++ swift/base/bytebuffer.h
@@ -0,0 +1,161 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __SWIFT_BASE_BYTE_BUFFER_H__
+#define __SWIFT_BASE_BYTE_BUFFER_H__
+
+#include <string>
+
+#include "swift/base/noncopyable.hpp"
+#include "swift/base/byteorderhelper.h"
+
+namespace swift {
+    
+class ByteBuffer : swift::noncopyable
+{
+    typedef ByteOrderHelper::ByteOrder ByteOrder;
+public:
+    class ReadPosition
+    {
+        friend class ByteBuffer;
+        ReadPosition (size_t start, int version) 
+            : start_ (start)
+            , version_ (version)
+        {
+        }
+
+        size_t start_;
+        int version_;
+    };
+public:
+    // Note: the default byte's order is ByteOrder::BYTE_ORDER_HOST
+    ByteBuffer ();
+    explicit ByteBuffer (const char *buffer);
+    explicit ByteBuffer (ByteOrder byte_order);
+    ByteBuffer (const char *buffer, size_t length);
+    ByteBuffer (const char *buffer, 
+                size_t length, 
+                ByteOrder byte_order);
+    
+    ~ByteBuffer ();
+
+    //movable
+    ByteBuffer (ByteBuffer&& other);
+    ByteBuffer& operator= (ByteBuffer&& rhs);
+
+    size_t Length () const
+    {
+        return end_ - start_;
+    }
+
+    size_t Capacity () const
+    {
+        return size_ - start_;
+    }
+
+    const char* Data () const
+    {
+        return buffer_ + start_;
+    }
+
+    ByteOrder Order () const
+    {
+        return byte_order_;
+    }
+
+    ReadPosition GetReadPosition () const
+    {
+        return ReadPosition (start_, version_);
+    }
+
+    // if the version in new position isn't equal to version_, return false. 
+    bool SetReadPosition (const ReadPosition& position);
+
+    std::string ToString () const
+    {
+        return std::string (buffer_ + start_, end_ - start_);
+    }
+
+    // Read a next value from the buffer. Return false if 
+    // there isn't enough data left for the specified type.
+    bool ReadUInt (uint8_t *val)
+    {
+        if (nullptr == val) { return false;}
+        return ReadBytes (reinterpret_cast<char*>(val), 1);
+    }
+
+    bool ReadUInt16 (uint16_t *val);
+    bool ReadUInt32 (uint32_t *val);
+    bool ReadUInt64 (uint64_t *val);
+    bool ReadBytes (char *val, size_t len);
+
+    // Appends next len bytes to val, if there is less than len return false.
+    bool ReadString (std::string *val, size_t len);
+
+    // Write value to the buffer. auto resizes the buffer when it is necessary.
+    void WriteUInt8 (uint8_t val)
+    {
+        WriteBytes (reinterpret_cast<const char*>(&val), 1);
+    }
+
+    void WriteUInt16 (uint16_t val);
+    void WriteUInt32 (uint32_t val);
+    void WriteUInt64 (uint64_t val);
+
+    void WriteString (const std::string& val)
+    {
+        WriteBytes (val.c_str (), val.size ());
+    }
+
+    void WriteBytes (const char* val, size_t len);
+
+    // Resize the buffer to the specified size.
+    // This invalidates any remembered seek positions.
+    void Resize (size_t size);
+
+    // Reserves the given number of bytes and return a char* that can be
+    // written into. Useful for functions that require a char* buffer and
+    // not a ByteBuffer.
+    char* ReserveWriteBuffer (size_t len);
+
+    // Moves current position size bytes forward. Return false if there is
+    // less than size bytes left in the buffer. Consume doesn't permanently
+    // remove data. so remembered read positions are still valid after this call.
+    bool Consume (size_t size);
+
+    // Clears the contents of the buffer. After this, Length() will be 0.
+    void Clear ();
+
+    void Swap (ByteBuffer& other);
+
+private:
+    void Initialize (const char *buffer, 
+                     size_t size, 
+                     ByteOrder byte_order);
+
+private:
+    char* buffer_;
+    size_t size_;
+    size_t start_;
+    size_t end_;
+    int version_;
+    ByteOrder byte_order_;
+
+private:
+    static const int kDefaultSize = 4096;
+};
+
+} // namespace swift
+
+#endif // __SWIFT_BASE_BYTE_BUFFER_H__

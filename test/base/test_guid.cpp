c5d36898f80dcdfab63362779f8154682448b3a9
chen.yongwang <chen.yongwang@apusapp.com>
2014-06-27 23:15:33 +0800

    Add Guid unit test

---
+++ test/base/test_guid.cpp
@@ -0,0 +1,69 @@
+#include <gtest/gtest.h>
+#include <swift/base/guid.h>
+
+#include <thread>
+#include <set>
+#include <vector>
+#include <mutex>
+
+class test_Guid : public testing::Test
+{
+public:
+    test_Guid();
+    ~test_Guid();
+};
+
+TEST(test_Guid, Zeroes)
+{
+    uint64_t bytes[] = { 0, 0 };
+    std::string guid;
+    EXPECT_TRUE(swift::Guid::RandomDataToGuidString(bytes, guid));
+    EXPECT_EQ(guid, "00000000-0000-0000-0000-000000000000");
+}
+
+TEST(test_Guid, Correctly)
+{
+    uint64_t bytes[] = { 0x0123456789ABCDEFULL, 0xFEDCBA9876543210ULL };
+    std::string guid;
+    EXPECT_TRUE(swift::Guid::RandomDataToGuidString(bytes, guid));
+    EXPECT_EQ(guid, "01234567-89AB-CDEF-FEDC-BA9876543210");
+
+    for (int i = 0; i < 100; ++i) {
+        std::move(swift::Guid::Generate(guid));
+        EXPECT_TRUE(swift::Guid::IsValidGuid(guid));
+    }
+}
+
+TEST(test_Guid, Uniqueness)
+{
+    std::string guid1;
+    std::string guid2;
+    for (int i = 0; i < 100; ++i) {
+        swift::Guid::Generate(guid1);
+        swift::Guid::Generate(guid2);
+        EXPECT_NE(guid1, guid2);
+        EXPECT_EQ(guid1.size(), 36);
+        EXPECT_EQ(guid2.size(), 36);
+    }
+
+    std::mutex mu;
+    std::set<std::string> ss;
+    std::vector<std::thread> ths;
+    for (int i = 0; i < 4; ++i) {
+        ths.push_back(std::move(std::thread([&mu, &ss]{
+            std::string guid;
+            for (int i = 0; i < 10000; ++i) {
+                EXPECT_TRUE(swift::Guid::Generate(guid));
+                EXPECT_TRUE(swift::Guid::IsValidGuid(guid));
+                mu.lock();
+                auto ret = ss.insert(std::move(guid));
+                mu.unlock();
+                EXPECT_NE(ret.second, false);
+            }
+        })));
+    }
+
+    for (auto&& i : ths) {
+        i.join();
+    }
+}
